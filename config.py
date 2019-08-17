23ff3e4e7c7d2f53baa02e3bf0e6357b175e5fff
SJ <seung-jae.bang@spglobal.com>
2019-08-17 13:54:28 -0400

    add helper files to read ingested data

---
+++ config.py
@@ -0,0 +1,2 @@
+STOCKTWITS_ROOT_DIR = '/Users/seung-jae_bang/Personal/Research/Stock_Sentiment/data/Stocktwits'
+# STOCKDATA_ROOT_DIR =
---
+++ util/file_util.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+from glob import glob
+import os
+
+import pandas as pd
+
+from config import STOCKTWITS_ROOT_DIR
+
+
+_STOCK_TWIT_FILE_FORMAT = '{ticker}_{date_str}.json'
+_STOCK_TWIT_FILE_FORMAT_ALL = '{ticker}_*.json'
+
+_DEFAULT_COLS = [
+    'date_est',
+    'created_at_est',
+    'body',
+    'symbols',
+    'entities.sentiment.basic',
+    'links',
+]
+
+
+class StockTwitsFileReader:
+
+    def __init__(self, root_dir=STOCKTWITS_ROOT_DIR):
+        self.root_dir = root_dir
+
+    def get_root_dir(self):
+        return self.root_dir
+
+    def get_subdir_for_ticker(self, ticker):
+        return os.path.join(self.root_dir, ticker)
+
+    def get_all_files_for_ticker(self, ticker):
+        subdir = self.get_subdir_for_ticker(ticker)
+        file_paths = glob(os.path.join(subdir,
+                                       _STOCK_TWIT_FILE_FORMAT_ALL.format(ticker=ticker)))
+        return file_paths
+
+    def _validate_date_format(self, date):
+        """Validate that date is in format YYYY-MM-DD"""
+        try:
+            datetime.strptime(date, '%Y-%m-%d')
+        except ValueError:
+            raise ValueError('date {} is in incorrect format'.format(date))
+
+    def get_twit_file_path(self, ticker, date):
+        self._validate_date_format(date)
+
+        return os.path.join(self.get_subdir_for_ticker(ticker),
+                            _STOCK_TWIT_FILE_FORMAT.format(ticker=ticker, date_str=date))
+
+    def get_twit_file_paths_in_range(self, ticker, start_date, end_date):
+        self._validate_date_format(start_date)
+        self._validate_date_format(end_date)
+
+        candidate_file_paths = self.get_all_files_for_ticker(ticker)
+        candidate_file_paths = [(f, f.split('_')[-1].split('.')[0]) for f in candidate_file_paths]
+        file_paths = [x for x in candidate_file_paths if start_date <= x[1] <= end_date]
+        file_paths = sorted(file_paths, key=lambda x: x[1])
+        file_paths = [f[0] for f in file_paths]
+        return file_paths
+
+    def read_json_file_to_df(self, file_path):
+        return pd.read_json(file_path, orient='records', lines=True)
+
+    def read_twit_file(self, ticker, date, cols='all'):
+        file_path = self.get_twit_file_path(ticker, date)
+        df = self.read_json_file_to_df(file_path)
+
+        if cols == 'default':
+            df = df[_DEFAULT_COLS].copy()
+        elif cols != 'all':
+            df = df[cols].copy()
+        return df
+
+    def read_twit_file_in_range(self, ticker, start_date, end_date, cols='all'):
+        file_paths = self.get_twit_file_paths_in_range(ticker, start_date, end_date)
+
+        dfs = []
+        for fp in file_paths:
+            dfs.append(self.read_json_file_to_df(fp))
+
+        result_df = pd.concat(dfs, sort=True).reset_index(drop=True)
+
+        if cols == 'default':
+            result_df = result_df[_DEFAULT_COLS].copy()
+        elif cols != 'all':
+            result_df = result_df[cols].copy()
+        return result_df

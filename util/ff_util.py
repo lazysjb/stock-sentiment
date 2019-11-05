62d43ff145ce8c540a0fe4d3e85f16a75f92547d
lazysjb <sb2927@columbia.edu>
2019-11-04 19:57:17 -0500

    naive portfolio strategy and regress on fama french factors

---
+++ util/ff_util.py
@@ -0,0 +1,56 @@
+"""Util to process Fama French factors"""
+import os
+
+import pandas as pd
+
+FF_DAILY_RETURNS_FILEPATH = os.path.join(os.path.dirname(__file__),
+                                         '../data/F-F_Research_Data_Factors_daily.CSV')
+FF_DAILY_MOMENTUM_FILEPATH = os.path.join(os.path.dirname(__file__),
+                                          '../data/F-F_Momentum_Factor_daily.CSV')
+
+
+def read_ff_factors_daily(filepath=FF_DAILY_RETURNS_FILEPATH,
+                          include_momentum=True):
+    ff_df = pd.read_csv(filepath,
+                        skiprows=[0, 1, 2])
+    ff_df_cols = ff_df.columns.values
+    ff_df_cols[0] = 'date'
+    ff_df.columns = ff_df_cols
+
+    ff_df = ff_df.drop('RF', axis=1)
+    # last row has copyright info
+    ff_df = ff_df.iloc[:-1].copy()
+    ff_df['date'] = pd.to_datetime(ff_df['date'])
+
+    ff_df = ff_df.set_index('date').sort_index().copy()
+    # Original unit is in %, converting to raw units
+    ff_df = ff_df / 100
+
+    if include_momentum:
+        mom_df = read_momentum_factor_daily()
+        ff_df = ff_df.merge(mom_df, left_index=True, right_index=True)
+
+    return ff_df
+
+
+def read_momentum_factor_daily(filepath=FF_DAILY_MOMENTUM_FILEPATH):
+    mom_df = pd.read_csv(FF_DAILY_MOMENTUM_FILEPATH,
+                         skiprows=14,
+                         header=None,
+                         names=['date', 'Mom'])
+    # last row has copyright info
+    mom_df = mom_df.iloc[:-1].copy()
+    mom_df['date'] = pd.to_datetime(mom_df['date'])
+
+    mom_df = mom_df.set_index('date').sort_index().copy()
+    # Original unit is in %, converting to raw units
+    mom_df = mom_df / 100
+    return mom_df
+
+
+def get_ff_factors_with_freq(filepath=FF_DAILY_RETURNS_FILEPATH,
+                             freq='W-FRI'):
+    ff_df_daily = read_ff_factors_daily(filepath=filepath)
+    ff_df_temp = ff_df_daily + 1
+    result = ff_df_temp.resample(freq).apply(lambda g: g.prod(axis=0)) - 1
+    return result
--- util/plot_util.py
+++ util/plot_util.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
 
+from util.file_util import StockDataFileReader, StockTwitsFileReader
+from util.ts_util import resample_weekly
+
 
 def plot_time_series(df,
                      figsize=(8, 6),
@@ -55,4 +58,31 @@ def plot_time_series_bar(df,
 def overlay_on_secondary_axis(df, col_name, primary_ax):
     secondary_ax = primary_ax.twinx()
     _ = secondary_ax.plot(df.index, df[col_name], linewidth=0.7, linestyle='--')
+    return
+
+
+def plot_twit_series_for_ticker(ticker,
+                                start_date,
+                                end_date,
+                                overlay_price=True,
+                                freq='w'):
+    twit_file_reader = StockTwitsFileReader()
+    twit_df = twit_file_reader.read_daily_sentiment_summary_prelim(ticker,
+                                                                   start_date=start_date,
+                                                                   end_date=end_date)
+    if freq == 'w':
+        twit_df = resample_weekly(twit_df)
+    else:
+        raise ValueError('Unrecognized freq {}'.format(freq))
+
+    ax = plot_time_series_bar(twit_df,
+                              figsize=(14, 5), )
+
+    if overlay_price:
+        stock_file_reader = StockDataFileReader()
+        price_df = stock_file_reader.read_stockdata_in_range(ticker,
+                                                             start_date,
+                                                             end_date)
+        overlay_on_secondary_axis(price_df, 'adjusted close', ax)
+
     return

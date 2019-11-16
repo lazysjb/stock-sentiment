1e3023dc6a73c2759b002aa9df71e909b31c4844
lazysjb <sb2927@columbia.edu>
2019-11-16 16:40:41 -0500

    update gitignore and delete scrap file

--- dummy.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-file_path = ('/Users/seung-jae_bang/Personal/Research/Stock_Sentiment/'
-             'data/Stocktwits/processed/sentiment_summary_prelim/BYND_sentiment_summary.pkl')
-
-
-df = pd.read_pickle(file_path)
-
-a = 5
